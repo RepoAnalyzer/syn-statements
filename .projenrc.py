6d5099088dab6dea9066ecdd67fd36f974fd43ea
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-11-05 13:47:46 +0300

    [Build](Init): Made a simple structure via projen.

--- .projenrc.py
+++ .projenrc.py
@@ -1,11 +1,18 @@
 from projen.python import PythonProject
 
 project = PythonProject(
-    author_email="46250621+DeadlySquad13@users.noreply.github.com",
-    author_name="DeadlySquad13",
-    module_name="projen_test3",
-    name="projen-test3",
-    version="0.1.0",
+    author_email='46250621+DeadlySquad13@users.noreply.github.com',
+    author_name='DeadlySquad13',
+    module_name='projen_template',
+    name='projen_template',
+    version='0.1.0',
+
+    setuptools=True,
+
+    dev_deps=[
+        'pytest'
+        'black'
+    ],
 )
 
 project.synth()
---
+++ src/projen_test3/__main__.py
@@ -0,0 +1,5 @@
+from projen_test3.example import hello
+
+if __name__ == "__main__":
+    name = input("What is your name? ")
+    print(hello(name))
