ab1e3381d7c503f8cd2277c987ab976cbedb5096
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-11-05 20:15:13 +0300

    [Doc](Projen): Added desription to main features, added pre-commit to devDeps.

--- .projenrc.py
+++ .projenrc.py
@@ -20,81 +20,111 @@ class PythonRepoAnalyzerProject(PythonProject):
         self.gitignore.exclude(".venv_win")
 
         if black:
-            self.black = True
-            self.add_dev_dependency("black@^22")
-
+            self.add_black()
         if flake8:
-            self.flake8 = TomlFile(
-                self,
-                ".flake8",
-                obj={
-                    "flake8": {
-                        "max-line-length": 88,
-                        "extend-ignore": "E203",
-                    },
-                },
-            )
-
-            self.add_dev_dependency("flake8@^5")
-
+            self.add_flake8()
         if isort:
-            self.flake8 = TomlFile(
-                self,
-                ".isort.cfg",
-                obj={
-                    "settings": {
-                        "profile": "black",
-                    },
+            self.add_isort()
+        if pre_commit:
+            self.add_pre_commit()
+
+    def add_black(self):
+        """Add black to the project as a dev dependency.
+        """
+        self.black = True
+        self.add_dev_dependency("black@^22")
+
+    def add_flake8(self):
+        """Add flake8 to the project as a dev dependency.
+        Create a simple configuration file to prevent conflicts with black.
+        """
+        self.flake8 = TomlFile(
+            self,
+            ".flake8",
+            obj={
+                "flake8": {
+                    "max-line-length": 88,
+                    "extend-ignore": "E203",
+                },
+            },
+        )
+
+        self.add_dev_dependency("flake8@^5")
+
+    def add_isort(self):
+        """Add isort to the project as a dev dependenncy.
+        Create a simple configuration file to prevent conflicts with black.
+        """
+        self.isort = TomlFile(
+            self,
+            ".isort.cfg",
+            obj={
+                "settings": {
+                    "profile": "black",
+                },
+            },
+        )
+
+        self.add_dev_dependency("isort@^5")
+
+    def add_pre_commit(self):
+        """Add pre-commit the project as a dev dependenncy.
+        Add base configuration for installed tools. List of supported modules:
+            * Default:
+                - trailing-whitespace,
+                - end-of-file-fixer,
+                - check-yaml,
+                - check-added-large-files,
+
+            * black,
+            * flake8.
+        """
+        contents = {
+            # src and test files.
+            "files": rf"{ROOT_MODULE_DIR}\/.*|{ROOT_TEST_DIR}\/.*",
+            "repos": [
+                {
+                    "repo": "https://github.com/pre-commit/pre-commit-hooks",
+                    "rev": "v2.3.0",
+                    "hooks": [
+                        {"id": "trailing-whitespace"},
+                        {"id": "end-of-file-fixer"},
+                        {"id": "check-yaml"},
+                        {"id": "check-added-large-files"},
+                    ],
+                }
+            ],
+        }
+
+        if self.black:
+            contents["repos"].append(
+                {
+                    "repo": "https://github.com/psf/black",
+                    "rev": "22.10.0",
+                    "hooks": [
+                        {"id": "black"},
+                    ],
                 },
             )
 
-            self.add_dev_dependency("isort@^5")
-
-        if pre_commit:
-            contents = {
-                # src and test files.
-                "files": fr"{ROOT_MODULE_DIR}\/.*|{ROOT_TEST_DIR}\/.*",
-                "repos": [
-                    {
-                        "repo": "https://github.com/pre-commit/pre-commit-hooks",
-                        "rev": "v2.3.0",
-                        "hooks": [
-                            {"id": "trailing-whitespace"},
-                            {"id": "end-of-file-fixer"},
-                            {"id": "check-yaml"},
-                            {"id": "check-added-large-files"},
-                        ],
-                    }
-                ],
-            }
-
-            if self.black:
-                contents["repos"].append(
-                    {
-                        "repo": "https://github.com/psf/black",
-                        "rev": "22.10.0",
-                        "hooks": [
-                            {"id": "black"},
-                        ],
-                    },
-                )
-
-            if self.flake8:
-                contents["repos"].append(
-                    {
-                        "repo": "https://gitlab.com/pycqa/flake8",
-                        "rev": "5.0.4",
-                        "hooks": [
-                            {"id": "flake8"},
-                        ],
-                    },
-                )
-
-            self.pre_commit = YamlFile(
-                self,
-                ".pre-commit-config.yaml",
-                obj=contents,
+        if self.flake8:
+            contents["repos"].append(
+                {
+                    "repo": "https://gitlab.com/pycqa/flake8",
+                    "rev": "5.0.4",
+                    "hooks": [
+                        {"id": "flake8"},
+                    ],
+                },
             )
+
+        self.pre_commit = YamlFile(
+            self,
+            ".pre-commit-config.yaml",
+            obj=contents,
+        )
+
+        self.add_dev_dependency("pre-commit@^2")
 
 
 project = PythonRepoAnalyzerProject(
